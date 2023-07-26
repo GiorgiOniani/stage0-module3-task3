7aff8028f7d0a481a044fe637aa025ab6c05557c
GiorgiOniani <93796850+GiorgiOniani@users.noreply.github.com>
2023-07-26 17:21:05 +0400

    Update DeclaringVars.java

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -1,7 +1,8 @@
 package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
-    public static void main(String[] args) {
-        //write code here
-    }
+   public static void main(String[] args) {
+        int a = 10, b = 10, c = 10;
+        System.out.println(c);
+   }
 }
